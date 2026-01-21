18bbe8b3bdc9c965c1cfde6ecc2985f32f4b5c61
Gophy John <yiran.zhang40680-bihz@basischina.com>
2026-01-21 12:04:08 +0800

    cursor plz

--- polygon-builder.js
+++ polygon-builder.js
@@ -56,9 +56,14 @@ class PolygonBuilder {
    */
   getMousePos(e) {
     const rect = this.canvas.getBoundingClientRect();
+    
+    // Calculate the scale ratio between displayed size and actual canvas size
+    const scaleX = this.canvas.width / rect.width;
+    const scaleY = this.canvas.height / rect.height;
+    
     return {
-      x: e.clientX - rect.left,
-      y: e.clientY - rect.top
+      x: (e.clientX - rect.left) * scaleX,
+      y: (e.clientY - rect.top) * scaleY
     };
   }
 
